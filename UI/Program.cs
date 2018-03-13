5a6f64cf6752480b997097f202495fd865a10574
laerte <laerte.uliam@gmail.com>
2018-03-13 18:10:19 -0300

    Init

---
+++ UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CreationalLibrary.FluentBuilder;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = Pessoa.Novo
+                .Chama("Laerte Uliam")
+                .ResideEm("Jd. Jaraguá").Build();
+
+            Console.WriteLine(pessoa.ToString());
+            Console.Read();
+        }
+    }
+}
