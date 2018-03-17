557c6ee3a3c50ff432dcefdc67676d6ac37f40f2
laerte <laerte.uliam@gmail.com>
2018-03-17 11:57:15 -0300

    Faceted Builder

--- CreationalLibrary/FluentBuilder/Pessoa.cs
+++ CreationalLibrary/FluentBuilder/Pessoa.cs
@@ -41,4 +41,6 @@
             return (SELF)this;
         }
     }
+
+    
 }
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using CreationalLibrary.FluentBuilder;
+﻿using CreationalLibrary.FacetedBuilder;
+using System;
 
 namespace UI
 {
@@ -7,12 +7,37 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            var pessoa = Pessoa.Novo
+            FluentBuilder();
+            FacetedBuilder();
+            Console.Read();
+        }
+
+        private static void FacetedBuilder()
+        {
+            Console.WriteLine("--- Faceted Builder ---");
+            var pessoaBuilder = new PessoaBuilder();
+            var pessoa = pessoaBuilder
+                .Chama("João")
+                .Trabalha
+                    .Na("Microsoft")
+                    .Como("Engenheiro de Software")
+                    .Ganhando(10000)
+                .Reside
+                    .NoEndereco("Av Brasil", "Rio de Janeiro", "Brasil").Build();
+            
+            Console.WriteLine(pessoa.ToString());
+        }
+
+        private static void FluentBuilder()
+        {
+            Console.WriteLine("--- Fluent Builder ---");
+            var pessoaBuilder = CreationalLibrary.FluentBuilder.Pessoa.Novo;
+
+            var pessoa = pessoaBuilder
                 .Chama("Laerte Uliam")
                 .ResideEm("Jd. Jaraguá").Build();
 
             Console.WriteLine(pessoa.ToString());
-            Console.Read();
         }
     }
 }
