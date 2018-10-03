acdea0e4f6729402b468718f6bbef033b204512b
laerteuliam <laerte.uliam@gmail.com>
2018-10-03 17:27:58 -0300

    Singleton Dependency Injection Tests

--- CreationalLibrary.Tests/SingletonDataBaseTests.cs
+++ CreationalLibrary.Tests/SingletonDataBaseTests.cs
@@ -1,3 +1,4 @@
+using Autofac;
 using CreationalLibrary.Singleton;
 using Xunit;
 
@@ -11,6 +12,26 @@ namespace CreationalLibrary.Tests
             var db = SingletonDatabase.Instance;
             var db2 = SingletonDatabase.Instance;
             Assert.True(db.Equals(db2));
+        }
+
+        [Fact]
+        public void DISingletonDatabaseTest()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConfigurableDatabase>()
+                .As<IDatabase>()
+                .SingleInstance();
+
+            builder.RegisterType<ConfigurableRecordFinder>();
+
+            using (var c = builder.Build())
+            {
+                var recordFinder = c.Resolve<ConfigurableRecordFinder>();
+                var recordFinder2 = c.Resolve<ConfigurableRecordFinder>();
+
+                Assert.False(ReferenceEquals(recordFinder, recordFinder2));
+                Assert.True(ReferenceEquals(recordFinder.Database, recordFinder2.Database));
+            }
         }
     }
 }
--- CreationalLibrary/Singleton/SingletonDatabase.cs
+++ CreationalLibrary/Singleton/SingletonDatabase.cs
@@ -10,6 +10,70 @@ namespace CreationalLibrary.Singleton
     {
         int GetPopulation(string name);
     }
+
+    public class ConfigurableDatabase : IDatabase
+    {
+        public int GetPopulation(string name)
+        {
+            return new Dictionary<string, int>
+            {
+                ["Mexico City"] = 20000,
+                ["Salvador"] = 23000,
+                ["Curitiba"] = 1000
+            }[name];
+        }
+    }
+
+    public class ConfigurableRecordFinder
+    {
+        private readonly IDatabase _database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            _database = database ?? throw new ArgumentNullException(paramName: nameof(database));
+        }
+
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            names.ForEach(name =>
+            {
+                result += _database.GetPopulation(name);
+            });
+            return result;
+        }
+
+        public IDatabase Database => _database;
+    }
+
+    public class OrdinaryDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static Lazy<OrdinaryDatabase> instance = new Lazy<OrdinaryDatabase>(() =>
+        {
+            return new OrdinaryDatabase();
+        }
+        );
+        public static IDatabase Instance => instance.Value;
+
+        private OrdinaryDatabase()
+        {
+            Console.WriteLine("Initialiazing Database");
+            var pathFile = Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "capitals.txt");
+            capitals = File.ReadAllLines(pathFile)
+                    .Batch(2)
+                    .ToDictionary(
+                        list => list.ElementAt(0).Trim(),
+                        list => int.Parse(list.ElementAt(1))
+                    );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+
     public class SingletonDatabase : IDatabase
     {
         private Dictionary<string, int> capitals;
